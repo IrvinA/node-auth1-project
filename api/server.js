05b0616c2e0268edf92f3be2dab6eff6bdf7e1cc
IrvinA <arevalosirvin@gmail.com>
2021-12-13 09:19:32 -0800

    index and server.js complete

--- api/server.js
+++ api/server.js
@@ -1,31 +1,53 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-/**
-  Do what needs to be done to support sessions with the `express-session` package!
-  To respect users' privacy, do NOT send them a cookie unless they log in.
-  This is achieved by setting 'saveUninitialized' to false, and by not
-  changing the `req.session` object unless the user authenticates.
+const session = require('express-session');
 
-  Users that do authenticate should have a session persisted on the server,
-  and a cookie set on the client. The name of the cookie should be "chocolatechip".
+const Store = require('connect-session-knex')(session);
 
-  The session can be persisted in memory (would not be adecuate for production)
-  or you can use a session store like `connect-session-knex`.
- */
+const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(
+  session({
+    name: 'chocolatechip',
+    secret: process.env.SESSION_SECRET || 'Himitsu',
+    cookie: {
+      maxAge: 1000 * 120,
+      secure: false,
+      httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new Store({
+      knex: require('../data/db-config'),
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+      createtable: true,
+      clearInterval: 1000 * 60 * 60,
+    }),
+  })
+);
 
-server.get("/", (req, res) => {
-  res.json({ api: "up" });
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+  res.json({ api: 'up' });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use('*', (req, res, next) => {
+  next({ status: 404, message: 'not found' });
+});
+
+// eslint-disable-next-line no-unused-vars
+server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
